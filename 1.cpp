5cc9dce6a4179defcc5ead41e0af60763cbcad7a
sukihan <851682771@qq.com>
2024-04-30 09:54:26 +0800

    commit:2

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class integer 
+{
+  private:
+    int num;
+  public:
+    integer(int n) : num(n) {}
+    integer() : num(0) {}
+    int get() { return num; }
+    integer operator+(integer other);
+    integer operator=(integer other);
+    ~integer() {}
+};
+
+integer integer::operator+(integer other)
+{
+  num = num + other.get();
+  return *this;
+}
+
+integer integer::operator=(integer other)
+{
+  num = other.get();
+  return *this;
+}
+
+int main ()
+{
+  integer a(3),b(5),c;
+  c = a+b;
+  cout << c.get() << endl;
+  return 0;
+}
